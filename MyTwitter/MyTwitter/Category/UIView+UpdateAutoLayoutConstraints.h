dde3f724c5428a0c052369a3a22fe0a12a319b75
Cristan <cristanz@yahoo-inc.com>
2015-10-01 17:02:15 -0700

    Import a good category class that helps to easy for changing a UIView's height/width constaints in runtime with Auto Layout.
    need this in the UITableViewCell to show/hide UIImageView.

---
+++ MyTwitter/MyTwitter/Category/UIView+UpdateAutoLayoutConstraints.h
@@ -0,0 +1,13 @@
+//
+//  UIView+UpdateAutoLayoutConstraints.h
+//  MyTwitter
+//
+//  Created by Cristan Zhang on 10/1/15.
+//  Copyright (c) 2015 FSManJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (UpdateAutoLayoutConstraints)
+
+@end
---
+++ MyTwitter/MyTwitter/Category/UIView+UpdateAutoLayoutConstraints.m
@@ -0,0 +1,13 @@
+//
+//  UIView+UpdateAutoLayoutConstraints.m
+//  MyTwitter
+//
+//  Created by Cristan Zhang on 10/1/15.
+//  Copyright (c) 2015 FSManJi. All rights reserved.
+//
+
+#import "UIView+UpdateAutoLayoutConstraints.h"
+
+@implementation UIView (UpdateAutoLayoutConstraints)
+
+@end
